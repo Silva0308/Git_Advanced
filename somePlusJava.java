ec04495c430aa62fe8a7642d30c0828de0748c56
Silva0308 <v.travinina@gmail.com>
2023-06-30 15:18:35 +0300

    added first mth

---
+++ somePlusJava.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class somePlusJava {
+    public static int inputNum() {
+        Scanner iScanner = new Scanner(System.in);
+        int num;
+        do {
+            System.out.println("Введите целое число: ");
+            while (!iScanner.hasNextInt()) {
+                System.out.println("Это не число!");
+                iScanner.next();
+            }
+            num = iScanner.nextInt();
+        } while (num <= 0);
+        iScanner.close();
+        return num;
+    }
+}
